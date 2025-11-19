4aaa7494da73cb5b82c9bd6776a60a65e0d0ce52
DanielRojas1920 <danielrojvilla@gmail.com>
2025-11-18 20:21:17 -0600

    fix date

--- initdb/init.sql
+++ initdb/init.sql
@@ -2,10 +2,11 @@ CREATE TABLE tasks (
     id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(100) NOT NULL,
     description TEXT,
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+    created_at DATE
 );
 
-INSERT INTO tasks (title, description) 
+INSERT INTO tasks (title, description, created_at) 
 VALUES 
-('Aprender Flask', 'Primer paso para dominar backend en Python'),
-('Hacer un CRUD', 'Implementar las operaciones básicas para tareas');
+('Aprender Flask', 'Primer paso para dominar backend en Python', '2025-02-15'),
+('Hacer un CRUD', 'Implementar las operaciones básicas para tareas', '2025-02-15');
+
